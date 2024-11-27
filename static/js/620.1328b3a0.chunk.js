accf4327cbbd66e05be862013e6f409fa945a84f
Ichim99 <140713412+Ichim99@users.noreply.github.com>
2024-11-27 11:17:42 +0200

    Updates

---
+++ static/js/620.1328b3a0.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkcamper_rent_project=self.webpackChunkcamper_rent_project||[]).push([[620],{2620:(t,e,r)=>{r.d(e,{rKS:()=>a});var L=r(3441);function a(t){return(0,L.k5)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{fill:"none",strokeWidth:"2",d:"M5,6 L1,4.5 L1,18.443038 L12,23 L23,18.443038 L23,4 L19,6 M5,16 L5,2 L12,5 L19,2 L19,16 L12,19 L5,16 Z M11.95,5 L11.95,19"},child:[]}]})(t)}}}]);
+//# sourceMappingURL=620.1328b3a0.chunk.js.map
